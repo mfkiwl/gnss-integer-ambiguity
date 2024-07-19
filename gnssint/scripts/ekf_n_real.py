e4ccd60ca729a447193b3d17b52a9d060bc0476d
Lejeune <clementlej@gmail.com>
2024-07-19 10:25:40 +0200

    uncomplete script ekf

--- gnssint/scripts/ekf_n_real.py
+++ gnssint/scripts/ekf_n_real.py
@@ -1,6 +1,7 @@
 from filterpy.kalman import ExtendedKalmanFilter
 import numpy as np
 from gnssint.data import constants
+from gnssint.data.utils import MeasurementSamples
 
 # e.g position receiver: [5.517579263180752,-2.4071475774665894]
 
